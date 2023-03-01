9f6ff64bc1a8ec9bea46556b877ccef0a44560c9
Redouane Ajgagal <redouaneajgagal1@gmail.com>
2023-03-01 22:59:48 +0100

    Fix filter inputs getting unchecked on toggle

--- src/components/invoices/FilterInput.tsx
+++ src/components/invoices/FilterInput.tsx
@@ -6,13 +6,17 @@ interface Props {
 }
 
 const FilterInput: React.FC<Props> = ({ name }) => {
-    const { filterInvoice } = useContext(FilterStatus);
+    const { filterInvoice, isDraft, isPaid, isPending } = useContext(FilterStatus);
     const filterStatusHanlder = () => {
         filterInvoice(name)
     }
+    let isChecked = false
+    if (name === 'paid' && isPaid || name === 'draft' && isDraft || name === 'pending' && isPending) {
+        isChecked = true
+    }
     return (
         <label htmlFor={name} className='flex gap-2 cursor-pointer font-medium'>
-            <input onChange={filterStatusHanlder} type="checkbox" name={name} id={name} className='cursor-pointer accent-primaryPurple w-full max-w-[.9rem]' />
+            <input onChange={filterStatusHanlder} type="checkbox" name={name} id={name} checked={isChecked} className='cursor-pointer accent-primaryPurple w-full max-w-[.9rem]' />
             {name}
         </label>
     )
